bd25af82f80798a747326b752d5c15831e382834
Ivan Strelkov <iv-strelkov@yandex.ru>
2018-02-19 17:25:59 +0300

    Demo 2 - find imported props demo

---
+++ demo-2-unused-exports/scripts/custom-walker.js
@@ -0,0 +1,31 @@
+const nodeMetaKeys = ['loc', 'range', 'start', 'end', 'type', 'parent'];
+
+function walkNode(node, callback) {
+  // Проверяем, что находимся внутри
+  if (!node.type) {
+    return;
+  }
+
+  callback(node);
+
+  for (const key in node) {
+    // Отбрасываем ключи, которые заведомо не являются нодами
+    if (nodeMetaKeys.indexOf(key) !== -1) {
+      continue;
+    }
+
+    const potentialChildNode = node[key];
+
+    if (Array.isArray(potentialChildNode)) {
+      // Если это массив нод - вызываем итератор для каждого ребенка, передав туда parent
+      potentialChildNode.forEach(function (potentialDescendantNode) {
+        walkNode({ ...potentialDescendantNode, parent: node }, callback);
+      });
+    } else if (typeof potentialChildNode === 'object') {
+      // Если же это объект (предположительно - нода) - вызываем итератор для самого объекта
+      walkNode({ ...potentialChildNode, parent: node }, callback);
+    }
+  }
+}
+
+module.exports = walkNode;
